b5609bff353f0adc44ad824563dba4f83a4f7abd
ppz1 <133365100+pedrowAguilera@users.noreply.github.com>
2024-04-12 14:49:02 -0300

    Initial commit

---
+++ Exercicio1/Exercicio1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+//ATIVIDADE 1
+//int main() {
+//	float instalacao = 300.00;
+//	float limpeza = 150.00;
+//	double manutencao = 499.99;
+//
+//	double valor = instalacao + limpeza + manutencao;
+//
+//	cout << "Valor a ser pago: ";
+//	cout << valor;
+//
+//	return 0;
+//}
+
+
+//ATIVIDADE 2
+//int main() {
+//	int recebe = 35;
+//	int horario = 8;
+//
+//	int salario = recebe * horario;
+//	
+//	cout << "Salario por dia: ";
+//	cout << salario;
+//
+//	return 0;
+//}
+
+
+//ATIVIDADE 3
+//int main() {
+//	int bombom = 27;
+//	int caixas = 16;
+//
+//	int total = bombom * caixas;
+//
+//	cout << "Quantidade de bombons: ";
+//	cout << total;
+//
+//	return 0;
+//}
+
+
+//ATIVIDADE 4
+//int main() {
+//	int garrafas = 300;
+//	int caixas = 20;
+//
+//	int vendidos = garrafas / caixas;
+//
+//	cout << "Caixas usadas: ";
+//	cout << vendidos;
+//
+//	return 0;
+//}
+
+
+//ATIVIDADE 5
+//int main() {
+//	int camisetas1 = 120;
+//	int vendas1 = 80;
+//	int valorvendas = 30;
+//	int camisetas2 = 50;
+//
+//	int faturamento = vendas1 * valorvendas;
+//	int quantidade = camisetas1 - vendas1 + camisetas2;
+//
+//	cout << "Camisetas restantes: ";
+//	cout << quantidade;
+//	cout << "\n";
+//	cout << "Faturamento: "; 
+//	cout << faturamento;
+//}
